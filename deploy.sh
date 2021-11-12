c35fb2d7df103cc27c3e8f8be11e0acfc6cbd7ec
Akintola <lonlondevelop@gmail.com>
2021-11-12 17:26:48 +0100

    fix: fix typos

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ docker build -t akintola/multi-server:latest -t akintola/multi-server:$SHA -f ./
 docker build -t akintola/multi-worker:latest -t akintola/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push akintola/multi-client:latest
-docker push akintola/multi-server
-docker push akintola/multi-worker
+docker push akintola/multi-server:latest
+docker push akintola/multi-worker:latest
 
 docker push akintola/multi-client:$SHA
 docker push akintola/multi-server:$SHA
